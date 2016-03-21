24796157994e5ba50b4d76342878c5658d0b1440
Felipe Gavilan <elmejor_887@hotmail.com>
2016-03-21 19:13:54 -0400

    Initial Commit

---
+++ PiedraPapelTijeras.WPF.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PiedraPapelTijeras.WPF.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

3b9190b637c4b8b645e48210fa4b5c84c718bb6a
Felipe Gavilan <elmejor_887@hotmail.com>
2016-03-25 18:02:43 -0400

    Logica del juego implementada.

--- PiedraPapelTijeras.WPF/MainWindow.xaml.cs
+++ PiedraPapelTijeras.WPF/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using PiedraPapelTijeras.Core;
+using PiedraPapelTijeras.Core.Models;
+using PiedraPapelTijeras.WPF.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,14 +18,78 @@ using System.Windows.Shapes;
 
 namespace PiedraPapelTijeras.WPF
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Marcador> _marcador;
+        private Random _rnd;
         public MainWindow()
         {
             InitializeComponent();
+            _marcador = Marcador.ObtenerMarcador(2);
+            gridMarcador.ItemsSource = _marcador;
+            _rnd = new Random(Environment.TickCount);
+        }
+
+        private void imgPiedra_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Jugar(new Piedra());
+        }
+
+        private void imgPapel_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Jugar(new Papel());
+        }
+
+        private void imgTijera_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Jugar(new Tijera());
+        }
+
+        private void Jugar(Eleccion eleccionDelJugador1)
+        {
+            var jugada1 = new Jugada() { Id = 1, Eleccion = eleccionDelJugador1 };
+
+            var eleccionDelJugador2 = EleccionAleatoria();
+
+            var jugada2 = new Jugada() { Id = 2, Eleccion = eleccionDelJugador2 };
+
+            var jugadas = new List<Jugada>()
+            {
+                jugada1, jugada2
+            };
+
+            var decideGanador = DecideGanador.Decidir(jugadas);
+
+            ActualizarPuntaje(decideGanador);
+        }
+
+        private Eleccion EleccionAleatoria()
+        {
+            var dictElecciones = new Dictionary<int, Eleccion>()
+            {
+                {1, new Piedra()},
+                {2, new Papel()},
+                {3, new Tijera()}
+            };
+
+            int numeroAleatorio = _rnd.Next(1, 4);
+
+            return dictElecciones[numeroAleatorio];
+        }
+
+        private void ActualizarPuntaje(ResultadoJugada decideGanador)
+        {
+            if (decideGanador.Resultado == Resultado.Ganado)
+            {
+                _marcador[0].Puntuacion++;
+            }
+            else if (decideGanador.Resultado == Resultado.Perdido)
+            {
+                _marcador[1].Puntuacion++;
+            }
+
+            gridMarcador.ItemsSource = null;
+            gridMarcador.ItemsSource = _marcador;
         }
     }
 }
---
+++ PiedraPapelTijeras.WPF/Models/Marcador.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiedraPapelTijeras.WPF.Models
+{
+    public class Marcador
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public int Puntuacion { get; set; }
+
+        public static List<Marcador> ObtenerMarcador(int jugadores)
+        {
+            var listadoMarcador = new List<Marcador>()
+            {
+                new Marcador()
+                {
+                    Id = 1,
+                    Description = "Tú",
+                    Puntuacion = 0
+                },
+                new Marcador()
+                {
+                    Id = 2,
+                    Description = "Jugador 2",
+                    Puntuacion = 0
+                }
+            };
+
+            if (jugadores == 3)
+            {
+                listadoMarcador.Add(new Marcador()
+                {
+                    Id = 2,
+                    Description = "Jugador 3",
+                    Puntuacion = 0
+                });
+            }
+
+            return listadoMarcador;
+
+        }
+
+    }
+
+}

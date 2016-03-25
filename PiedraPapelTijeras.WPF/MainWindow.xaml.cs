1beb82ccb4d42188d522941b5887547f059264a3
Felipe Gavilan <elmejor_887@hotmail.com>
2016-03-25 18:34:50 -0400

    Remuevo usings no utilizados

--- PiedraPapelTijeras.WPF/MainWindow.xaml.cs
+++ PiedraPapelTijeras.WPF/MainWindow.xaml.cs
@@ -2,20 +2,9 @@
 using PiedraPapelTijeras.Core.Models;
 using PiedraPapelTijeras.WPF.Models;
 using PiedraPapelTijeras.WPF.Servicios;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PiedraPapelTijeras.WPF
 {
